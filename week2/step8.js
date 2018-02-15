3887fb088448d89ff6f4be8f1aa3e59fce8b08a7
Ghenowa <35230172+Ghenowa@users.noreply.github.com>
2018-02-15 09:59:30 +0100

    homework

---
+++ week2/step8.js
@@ -0,0 +1,40 @@
+> let x=5;
+undefined
+> let y=9;
+undefined
+> let z="hi";
+undefined
+> console.log("the value of my variable x is :" +x);
+the value of my variable x is :5
+undefined
+> console.log("the value of my variable y is :" +y);
+the value of my variable y is :9
+undefined
+> console.log("the value of my variable z is :" +z);
+the value of my variable z is :hi
+undefined
+> console.log(typeof x);
+number
+undefined
+> console.log(typeof y);
+number
+undefined
+> console.log(typeof z);
+string
+undefined
+> if(typeof x == typeof y){
+... console.log("the same type");}else{
+... console.log("the different type");}
+the same type
+undefined
+> if(typeof x == typeof z){
+... console.log("the same type");}else{
+... console.log("the different type");}
+the different type
+undefined
+> if(typeof y == typeof z){
+... console.log("the same type");}else{
+... console.log("the different type");}
+the different type
+undefined
+>

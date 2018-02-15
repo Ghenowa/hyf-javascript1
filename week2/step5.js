4143862846b68347cf2534fe1d534ac14e31146d
Ghenowa <35230172+Ghenowa@users.noreply.github.com>
2018-02-15 11:56:26 +0100

    homework

---
+++ week2/step5.js
@@ -0,0 +1,16 @@
+> let z=7.25;
+undefined
+> console.log(z);
+7.25
+undefined
+> let a= Math.round(z);
+undefined
+> console.log(a);
+7
+undefined
+> let Area= Math.max(a,z);
+undefined
+> console.log(Area);
+7.25
+undefined
+>

2997375848e4184b48b5329387b5edf528b845a2
Ghenowa <35230172+Ghenowa@users.noreply.github.com>
2018-02-15 12:07:33 +0100

    homework

---
+++ week2/step3.js
@@ -0,0 +1,17 @@
+> var x;
+undefined
+> console.log("the value of my variable x will be : undefinded");
+the value of my variable x will be : undefinded
+undefined
+> console.log(x);
+undefined
+undefined
+> x=20;
+20
+> console.log("the value of my variable x will be : number");
+the value of my variable x will be : number
+undefined
+> console.log(x);
+20
+undefined
+>

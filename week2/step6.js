bc0af52f9bdc715e709ff26985fad6526923000b
Ghenowa <35230172+Ghenowa@users.noreply.github.com>
2018-02-15 11:37:34 +0100

    homework

---
+++ week2/step6.js
@@ -0,0 +1,23 @@
+> var zoo=[];
+undefined
+> console.log("the value of my array will be : empty");
+the value of my array will be : empty
+undefined
+> console.log(zoo);
+[]
+undefined
+> var animals=["cat","dog","duck"];
+undefined
+> console.log(animals);
+[ 'cat', 'dog', 'duck' ]
+undefined
+> var daanfav="baby pig";
+undefined
+> console.log(daanfav);
+baby pig
+undefined
+> var zoo=animals. concat(daanfav);
+undefined
+> console.log(zoo);
+[ 'cat', 'dog', 'duck', 'baby pig' ]
+undefined

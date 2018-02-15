eeb6ac402ff146850383130f35828dd4535faca0
Ghenowa <35230172+Ghenowa@users.noreply.github.com>
2018-02-15 12:02:47 +0100

    homework

---
+++ week2/step4.js
@@ -0,0 +1,17 @@
+> var y="hello world";
+undefined
+> console.log("the value of my string will b e : hello world");
+the value of my string will b e : hello world
+undefined
+> console.log(y);
+hello world
+undefined
+> var y="hi";
+undefined
+> console.log("the value of my string will b e : hi");
+the value of my string will b e : hi
+undefined
+> console.log(y);
+hi
+undefined
+>

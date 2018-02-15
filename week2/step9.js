c926875783f8475df3186bc0d9e457325bcb26e3
Ghenowa <35230172+Ghenowa@users.noreply.github.com>
2018-02-15 09:22:04 +0100

    homework

---
+++ week2/step9.js
@@ -0,0 +1,24 @@
+> let x=7;
+undefined
+> x=x%2;
+1
+> console.log("the new value of x is :" +x);
+the new value of x is :1
+undefined
+> let y=9;
+undefined
+> y=y%6;
+3
+> console.log("the new value of y is " +y);
+the new value of y is 3
+undefined
+> let z=5;
+undefined
+> z=z%2;
+1
+> console.log("the new value of z is" +z);
+the new value of z is1
+
+
+
+ 
